9a9785eb0ce54c3d09affe3969904d111a7a2656
CM <christofferjcm@gmail.com>
2020-09-16 15:26:35 +0200

    Profile page

---
+++ DoggieDate/Controllers/ApplicationUserController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DoggieDate.Data;
+using DoggieDate.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DoggieDate.Controllers
+{
+    public class ApplicationUserController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private UserManager<ApplicationUser> _userManager;
+
+        public ApplicationUserController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            ApplicationUser user = _userManager.GetUserAsync(User).Result;
+            return View(user);
+        }
+    }
+}
