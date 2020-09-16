736df6508b70edd65cfef9b22d83a953dede1da0
CM <christofferjcm@gmail.com>
2020-09-16 16:11:10 +0200

    messages scaffold

--- DoggieDate/Controllers/ApplicationUserController.cs
+++ DoggieDate/Controllers/ApplicationUserController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using DoggieDate.Data;
 using DoggieDate.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -23,6 +24,21 @@ namespace DoggieDate.Controllers
         public IActionResult Index()
         {
             ApplicationUser user = _userManager.GetUserAsync(User).Result;
+            return View(user);
+        }
+
+        //[AllowAnonymous]
+        public IActionResult SearchUsers()
+        {
+            ApplicationUser user = _userManager.GetUserAsync(User).Result;
+
+            return View(user);
+        }
+
+        public IActionResult Messages()
+        {
+            ApplicationUser user = _userManager.GetUserAsync(User).Result;
+
             return View(user);
         }
     }
---
+++ DoggieDate/Controllers/MessagesController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DoggieDate.Data;
+using DoggieDate.Models;
+
+namespace DoggieDate.Controllers
+{
+    public class MessagesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MessagesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Messages
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Message.Include(m => m.Receiver).Include(m => m.Sender);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Messages/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var message = await _context.Message
+                .Include(m => m.Receiver)
+                .Include(m => m.Sender)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (message == null)
+            {
+                return NotFound();
+            }
+
+            return View(message);
+        }
+
+        // GET: Messages/Create
+        public IActionResult Create()
+        {
+            ViewData["ReceiverId"] = new SelectList(_context.User, "Id", "Id");
+            ViewData["SenderId"] = new SelectList(_context.User, "Id", "Id");
+            return View();
+        }
+
+        // POST: Messages/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,SenderId,ReceiverId,Content,TimeStamp,IsRead,Reported")] Message message)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(message);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ReceiverId"] = new SelectList(_context.User, "Id", "Id", message.ReceiverId);
+            ViewData["SenderId"] = new SelectList(_context.User, "Id", "Id", message.SenderId);
+            return View(message);
+        }
+
+        // GET: Messages/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var message = await _context.Message.FindAsync(id);
+            if (message == null)
+            {
+                return NotFound();
+            }
+            ViewData["ReceiverId"] = new SelectList(_context.User, "Id", "Id", message.ReceiverId);
+            ViewData["SenderId"] = new SelectList(_context.User, "Id", "Id", message.SenderId);
+            return View(message);
+        }
+
+        // POST: Messages/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,SenderId,ReceiverId,Content,TimeStamp,IsRead,Reported")] Message message)
+        {
+            if (id != message.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(message);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MessageExists(message.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ReceiverId"] = new SelectList(_context.User, "Id", "Id", message.ReceiverId);
+            ViewData["SenderId"] = new SelectList(_context.User, "Id", "Id", message.SenderId);
+            return View(message);
+        }
+
+        // GET: Messages/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var message = await _context.Message
+                .Include(m => m.Receiver)
+                .Include(m => m.Sender)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (message == null)
+            {
+                return NotFound();
+            }
+
+            return View(message);
+        }
+
+        // POST: Messages/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var message = await _context.Message.FindAsync(id);
+            _context.Message.Remove(message);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MessageExists(int id)
+        {
+            return _context.Message.Any(e => e.Id == id);
+        }
+    }
+}
